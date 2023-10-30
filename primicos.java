1ce5d141b2c04a4f8af8dadc05be07ba7345b445
Josep Tormo García <jostorgar4@edu.alu.gva.es>
2023-10-30 18:55:43 +0100

    Mejorado el programa que detecta números primos

--- primicos.java
+++ primicos.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class primicos {
     public static void main(String[] args) {
-        int num = 4;
+        int num = CastNum();
 
         switch (num) {
             case 2:
@@ -11,9 +13,27 @@ public class primicos {
                 break;
             default:
                 if (num % 2 == 0 | num % 3 == 0 | num % 5 == 0 | num % 7 == 0) System.out.println(num +" no es primo");
-                else System.out.println(num + "es primo");
+                else System.out.println(num + " es primo");
                 break;
         }
 
+
+    }
+
+    static int CastNum() {
+        Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        boolean valido = false;
+
+        while (!valido){
+            try {
+                num = scanner.nextInt();
+                valido = true;
+            }catch (Exception e){
+                scanner.next();
+                System.out.println("Introduce un numero valido");
+            }
+        }
+        return num;
     }
 }
